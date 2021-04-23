05ead182ef812f1e562b53caa5758666426c53d7
mercy021 <kinyanjuimercywangari@gmail.com>
2021-04-23 21:09:26 +0300

    first commit

---
+++ main/SETUP.ino
@@ -0,0 +1,8 @@
+void SETUP(){
+  pinMode(Red,INPUT);
+  pinMode(Red,OUTPUT);
+  pinMode(Amber,INPUT);
+  pinMode(Amber,OUTPUT);
+  pinMode(Green,INPUT);
+  pinMode(Green,OUTPUT);
+}
---
+++ main/main.ino
@@ -0,0 +1,14 @@
+int Red=4;
+int Amber=6;
+int Green=8;
+
+void setup(){
+  SETUP();
+  
+}
+void loop(){
+  
+  RED();
+  AMBER();
+  GREEN();
+}
